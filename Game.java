a5538ef0c5d25782403a0126cf328251f8b9f6c4
Oscar Garcia Abad <mutumuy@gmail.com>
2019-06-12 22:01:44 +0200

    Commit12 Corregido bug comando help

--- Game.java
+++ Game.java
@@ -174,11 +174,8 @@ public class Game
      */
     private void printHelp() 
     {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
-        System.out.println();
         System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        parser.showCommands();
     }
 
     /** 
